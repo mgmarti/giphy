af0e2c8794376d5d9b4cc3e171dd120e55a9cbe6
moma <mogimartinez@gmail.com>
2019-03-31 16:13:14 -0500

    rendered buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
     console.log('Page Ready!');
 
-    const queries = ['Llamas', 'Raccoons', 'Pandas', 'Tarsiers', 'Lemurs'];
-    
+    var queries = ["Llamas", "Raccoons", "Pandas", "Tarsiers", "Lemurs"];
+    console.log(queries);
+
+    function renderButtons() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < queries.length; i++)  {
+
+            var newButton = $("<button>");
+            newButton.addClass("animal-button");
+            newButton.attr("data-name", queries[i]);
+            newButton.text(queries[i]);
+
+            $("#button-area").append(newButton);
+        }
+    }
+
+    renderButtons();
+
+
+
 });
