53d329e8fead2e40b0fb9919c4bc4579ac59df62
moma <mogimartinez@gmail.com>
2019-04-03 18:20:25 -0500

    added function for image animation on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,16 +24,17 @@ $(document).ready(function() {
         queries.push($("#input-animal").val().trim())
         $("#button-area").empty();
         renderButtons();
+        console.log(queries);
      
         $("#input-animal").val("")
     });
 
-    $(".animal-button").on("click", function() {
-    // console.log("CLICK CLICK");
+    $(document.body).on("click", ".animal-button", function() {
+    console.log("CLICK CLICK");
 
     const animalName = $(this).attr("data-animal");
     const queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animalName + "&key=NWSi6hzZ4d8qNSQo03XgsTc82qbD6pEX";
+        animalName + "&key=NWSi6hzZ4d8qNSQo03XgsTc82qbD6pEX&limit=8";
 
             $.ajax({
                 url: queryURL,
@@ -45,8 +46,13 @@ $(document).ready(function() {
                 for (var i = 0; i < results.length; i++) {
 
                     const animalDiv = $("<div class='animalDiv'>");
-                    const p = $("<p>").text("Rating: " + results[i].rating);
-                    const animalImage = $("<img>").attr('src', results[i].images.fixed_height.url);
+                    const p = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
+                    const animalImage = $("<img>").attr('src', results[i].images.fixed_height_still.url);
+                    animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    animalImage.attr("data-animate", results[i].images.fixed_height.url);
+                    animalImage.attr("data-state", "still");
+                    animalImage.attr("class", "gif");
+
                     animalDiv.append(p);
                     animalDiv.append(animalImage);
         
@@ -55,8 +61,24 @@ $(document).ready(function() {
             })
     })
 
+    function changeState() {          
 
+        const state = $(this).attr("data-state");
+        const animate = $(this).attr("data-animate");
+        const still = $(this).attr("data-still");
 
+        if(state == "still") {
+            $(this).attr("src", animate);
+            $(this).attr("data-state", "animate");
+        }
+
+        else if(state == "animate") {
+            $(this).attr("src", still);
+            $(this).attr("data-state", "still");
+        }   
+    }
+
+    $(document).on("click", ".gif", changeState);
         
         
 });
